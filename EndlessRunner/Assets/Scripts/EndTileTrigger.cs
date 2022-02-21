aa69fb16aab3082f36279415cec6719bf0a9a41d
Zeshan Rasul <zeshanrasul@gmail.com>
2022-02-21 02:37:31 +0000

    Tiles generating as player progresses

---
+++ EndlessRunner/Assets/Scripts/EndTileTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTileTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GameObject tileSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MovePlayer movePlayer;
+        other.TryGetComponent<MovePlayer>(out movePlayer);
+        if (movePlayer != null)
+        {
+            SpawnNewTile();
+            Debug.Log("Spawning new tile");
+        }
+    }
+
+    void SpawnNewTile()
+    {
+        Instantiate(tilePrefab, tileSpawnPoint.transform.position, Quaternion.identity);
+    }
+}
